6944c2739d163615c9d5e8c5512100e6c2677372
Galakart <galakart.android@gmail.com>
2022-02-14 10:50:51 +0700

    fix static in gitignore + missing css

--- frontend_retro/forms.py
+++ frontend_retro/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+
 class PrivateMessageForm(forms.Form):
     subscriber_number = forms.IntegerField(label='Абонентский номер')
-    message = forms.CharField(label='Сообщение', max_length=900, widget=forms.Textarea)
+    message = forms.CharField(
+        label='Сообщение', max_length=900, widget=forms.Textarea)
