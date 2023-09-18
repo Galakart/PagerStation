eddbf2f13f728f826510b721df34f429b584e5b6
Galakart <galakart.android@gmail.com>
2023-09-18 17:35:13 +0700

    fix sqlite3 foreign_keys

--- backend/db/connection.py
+++ backend/db/connection.py
@@ -1,5 +1,6 @@
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
+from sqlalchemy.engine import Engine
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine("sqlite:///./database.db", connect_args={"check_same_thread": False})
@@ -12,3 +13,10 @@ def get_session():
         yield session
     finally:
         session.close()
+
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
